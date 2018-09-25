0e4e768ccca0d79d6d5c1b0be5f4cb32ee1460ba
Nikolas Tomaštík <nikolas.tsk@gmail.com>
2018-09-25 21:27:41 +0200

    druhé cvičenie commit added .gitignore for .idea

--- 02-cvicenie/wawjs-cvicenie02/test/array.js
+++ 02-cvicenie/wawjs-cvicenie02/test/array.js
@@ -1,13 +1,13 @@
 const assert = require("assert");
-describe("Array API exploration", function() {
+describe("Array API exploration", function () {
 
-    it("indexOf vs. includes", function() {
+    it("indexOf vs. includes", function () {
         let arr = [1, NaN, 2];
 
         assert(arr.includes(NaN), "Includes can find NaNs, uses SameValueZero");
         assert(arr.indexOf(NaN) === -1, "indexOf cannot find NaN");
     });
-    it("indexOfNaN - finding NaN index in Array", function() {
+    it("indexOfNaN - finding NaN index in Array", function () {
         let arr = [1, NaN, 2];
         // this works
         assert(indexOfNaN(arr) === 1);
@@ -20,7 +20,7 @@ describe("Array API exploration", function() {
         }
     });
 
-    it("TODO: indexOf(NaN) - indexOf with SameValueZero", function() {
+    it("indexOf(NaN) - indexOf with SameValueZero", function () {
         let arr = [1, NaN, 2, 0, -0];
 
         assert(indexOf(arr, 1) === 0);
@@ -30,20 +30,36 @@ describe("Array API exploration", function() {
         assert(indexOf(arr, -0) === 3);
 
         function indexOf(arr, what) {
-        	//TODO: 
+            if (Number.isNaN(what)) {
+                for (let i = 0; i < arr.length; i++) {
+                    if (Number.isNaN(arr[i]))
+                        return i;
+                }
+                return -1;
+            }
+
+            return arr.indexOf(what);
         }
     });
-    it("TODO: indexOf(NaN) - indexOf with SameValue", function() {
+    it("TODO: indexOf(NaN) - indexOf with SameValue", function () {
         let arr = [1, NaN, 2, 0, -0];
-       	
-       	assert(indexOf(arr, 1) === 0);
+
+        assert(indexOf(arr, 1) === 0);
 
         assert(indexOf(arr, NaN) === 1);
         assert(indexOf(arr, 0) === 3);
         assert(indexOf(arr, -0) === 4); //!
 
         function indexOf(arr, what) {
-            //TODO:
+            if (Number.isNaN(what)) {
+                return arr.findIndex((num) => Number.isNaN(num));
+            }
+
+            if (Object.is(what, -0)) {
+                return arr.findIndex((num) => Object.is(num, -0));
+            }
+
+            return arr.indexOf(what);
         }
     });
 
