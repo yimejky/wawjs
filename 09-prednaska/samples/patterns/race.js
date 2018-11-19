c111dcfbf2397e3fe3fb322776038857a7c88791
Martin Marko <marcus@gratex.com>
2018-11-19 20:52:06 +0100

    [FIX] #9, preklepy z prednasky a zle sample

--- 09-prednaska/samples/patterns/race.js
+++ 09-prednaska/samples/patterns/race.js
@@ -2,10 +2,7 @@ const { race } = require("async");
 
 const a = (cb) => setTimeout(() => cb(null, "a"), 3000);
 const b = (cb) => setTimeout(() => cb(null, "b"), 2000);
-const c = (cb) => setTimeout(() => {
-  console.log("c");
-  cb(null, "c");
-}, 8000);
+const c = (cb) => setTimeout(() => cb(null, "c"), 8000);
 
 race([
     a,
